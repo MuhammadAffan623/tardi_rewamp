a4f1733d03a697d730be62f13feb33601f25a9a1
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-02-03 00:32:46 +0500

    fixes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -17,13 +17,16 @@
 // }
 
 export function formatNumber(number: number) {
+  const format = (num: number) =>
+    num % 1 === 0 ? num.toFixed(0) : num.toFixed(2);
+
   if (number >= 1e9) {
-    return (number / 1e9).toFixed(2) + "B";
+    return format(number / 1e9) + "B";
   } else if (number >= 1e6) {
-    return (number / 1e6).toFixed(2) + "M";
+    return format(number / 1e6) + "M";
   } else if (number >= 1e3) {
-    return (number / 1e3).toFixed(2) + "K";
+    return format(number / 1e3) + "K";
   } else {
-    return number.toFixed(2).toString();
+    return number.toString();
   }
 }
